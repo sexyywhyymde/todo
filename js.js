2782e97cf8cd2914214f5f7996c85e249afc1034
Andrey <setikonelove@gmail.com>
2025-03-11 14:49:49 +0300

    add delete button

--- js.js
+++ js.js
@@ -4,15 +4,20 @@ const list = document.querySelector(".list");
 
 // Listeners
 document.addEventListener("DOMContentLoaded", () => {
-  const todos = JSON.parse(localStorage.getItem("todos"))||[]
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
   todos.forEach((todo) => {
     addNewTodo(todo.text);
   });
 });
 
-form.addEventListener("click", handleSubmit);
-
+form.addEventListener("submit", handleSubmit);
+list.addEventListener("click", (e) => {
+  const deleteButton = e.target.closest("button[name='delete']");
+  if (deleteButton) {
+    deleteButtonClick(e);
+  }
+});
 // Helpers
 function saveToLocalStorage() {
   const todos = [];
@@ -20,9 +25,17 @@ function saveToLocalStorage() {
   todoItems.forEach((todoItem) => {
     const todoText = document.querySelector(".todo-item");
     const todo = { text: todoText.textContent };
-    todos.push(todo)
+    todos.push(todo);
   });
   localStorage.setItem("todos", JSON.stringify(todos));
+}
+function removeToLocalStorage(text) {
+  if(!text)return
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const updatedTodos = todos.filter((todo) => {
+    return todo.text !== text;
+  });
+  console.log(updatedTodos);
 }
 
 function handleSubmit(e) {
@@ -34,8 +47,21 @@ function handleSubmit(e) {
 
 function addNewTodo(text) {
   const li = document.createElement("li");
-  li.innerHTML = `<span class="todo-item">${text}</span>`;
+  li.innerHTML = `<span class="todo-item">${text}</span>
+                   <button name="delete">Удалить</button>`;
   li.classList.add("todo-list-item");
   list.appendChild(li);
   saveToLocalStorage();
+}
+
+// Event Handlers
+function deleteButtonClick(e) {
+  const todoItem = e.target.closest(".todo-list-item");
+  console.log(todoItem)
+  if (todoItem) {
+    todoItem.remove();
+    const todo = todoItem.querySelector(".todo-item");
+    removeToLocalStorage(todo.text);
+    
+  }
 }
